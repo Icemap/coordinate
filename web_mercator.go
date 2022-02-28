8526d6ebfb26e526a09dce891eaead289f0cfde5
Cheese <cheese.javaee@gmail.com>
2022-03-01 00:37:50 +0800

    [Update] add WGS84 to web mercator tile number feature

--- web_mercator.go
+++ web_mercator.go
@@ -45,3 +45,25 @@ func WebMercatorToWGS84(src Coordinate) (Coordinate, error) {
 
 	return result, nil
 }
+
+// WGS84ToWebMercatorTile convert coordinate from WGS84 to web mercator tile number
+func WGS84ToWebMercatorTile(src Coordinate, level int) (int, int, error) {
+	if !src.Valid(WGS84) {
+		return 0, 0, fmt.Errorf("%s: [WGS84] %+v", InvalidParam, src)
+	}
+
+	webMercator, err := WGS84ToWebMercator(src)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	lineTileCount := 1 << level
+	levelTileWidth := WebMercatorWidth / float64(lineTileCount)
+	mapTileX, mapTileY := int(webMercator.X/levelTileWidth), int(webMercator.Y/levelTileWidth)
+
+	if mapTileX < 0 || mapTileY < 0 || mapTileX > lineTileCount || mapTileY > lineTileCount {
+		return 0, 0, fmt.Errorf("%s: [Web Mercator Tile] %+v", InvalidParam, src)
+	}
+
+	return mapTileX, mapTileY, nil
+}
--- web_mercator_test.go
+++ web_mercator_test.go
@@ -1,6 +1,7 @@
 package coordinate
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -8,5 +9,15 @@ func TestWGS84AndWebMercator(t *testing.T) {
 	err := testCoordinateConvertFunc(WGS84ToWebMercator, WebMercatorToWGS84)
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestWGS84ToWebMercatorTile(t *testing.T) {
+	x, y, err := WGS84ToWebMercatorTile(Coordinate{116.363665, 39.913441}, 15)
+	if err != nil {
+		t.Error(err)
+	}
+	if x != 26975 || y != 12415 {
+		t.Error(fmt.Errorf("x, y should be 26975, 12415"))
 	}
 }
