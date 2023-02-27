f2230cda1848c2887ab53bc4abc4b732449e408c
Icemap <qizhi.wang@pingcap.com>
2023-02-27 13:52:34 +0800

    [Update] add tile style for google map

--- tms.go
+++ tms.go
@@ -10,17 +10,20 @@ import (
 // Map URL
 const (
 	// GoogleSatelliteURL google.com/maps satellite map URL
-	GoogleSatelliteURL = "http://mt[0,1,2,3].google.com/vt/lyrs=y&hl=zh-CN&hl=zh-CN&gl=CN&x={x}&y={y}&z={z}&s=Gali"
+	GoogleSatelliteURL = "http://mt[0,1,2,3].google.com/vt/lyrs=y&x={x}&y={y}&z={z}&s=Gali"
 	// GoogleImageURL google.com/maps image map URL
-	GoogleImageURL = "http://mt[0,1,2,3].google.com/vt/lyrs=m&hl=zh-CN&hl=zh-CN&gl=CN&x={x}&y={y}&z={z}&s=Gali"
+	GoogleImageURL = "http://mt[0,1,2,3].google.com/vt/lyrs=m&gl=CN&x={x}&y={y}&z={z}&s=Gali"
 	// GoogleTerrainURL google.com/maps terrain map URL
-	GoogleTerrainURL = "http://mt[0,1,2,3].google.com/vt/lyrs=p&hl=zh-CN&hl=zh-CN&gl=CN&x={x}&y={y}&z={z}&s=Gali"
+	GoogleTerrainURL = "http://mt[0,1,2,3].google.com/vt/lyrs=p&gl=CN&x={x}&y={y}&z={z}&s=Gali"
 	// AMapSatelliteURL amap.com satellite map URL
 	AMapSatelliteURL = "http://webst0[1,2,3,4].is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}"
 	// AMapCoverURL amap.com cover map URL
 	AMapCoverURL = "http://webst0[1,2,3,4].is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zhcn&size=1&scale=1&style=8"
 	// AMapImageURL amap.com image map URL
 	AMapImageURL = "http://webrd0[1,2,3,4].is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}"
+
+	// GoogleWithoutLabelSuffix google map add this suffix to drop the label
+	GoogleWithoutLabelSuffix = "&apistyle=s.t%3A0%7Cs.e%3Al%7Cp.v%3Aoff"
 )
 
 // Map Type
@@ -39,8 +42,18 @@ const (
 	AMapImage = "AMapImage"
 )
 
-// WebMercatorTileToURL convert web mercator tile number to URL, get domain randomly
-func WebMercatorTileToURL(mapType string, x, y, z int) string {
+type TiltStyle struct {
+	// Google Specify Config
+
+	// Google map tiles without label
+	GoogleWithLabel bool
+}
+
+// WebMercatorTileToURLWithTiltStyle
+// Convert web mercator tile number to URL, get domain randomly.
+// And set the tilt style parameters.
+func WebMercatorTileToURLWithTiltStyle(mapType string, x, y, z int,
+	tiltStyle TiltStyle) string {
 	urlModel := ""
 	switch mapType {
 	case GoogleSatellite:
@@ -67,5 +80,17 @@ func WebMercatorTileToURL(mapType string, x, y, z int) string {
 	subArray := strings.Split(found, ",")
 	urlModel = string(reg.ReplaceAll([]byte(urlModel), []byte(subArray[rand.Intn(len(subArray))])))
 
+	// Google type
+	if mapType == GoogleSatellite || mapType == GoogleImage || mapType == GoogleTerrain {
+		if !tiltStyle.GoogleWithLabel {
+			urlModel += GoogleWithoutLabelSuffix
+		}
+	}
+
 	return urlModel
+}
+
+// WebMercatorTileToURL convert web mercator tile number to URL, get domain randomly
+func WebMercatorTileToURL(mapType string, x, y, z int) string {
+	return WebMercatorTileToURLWithTiltStyle(mapType, x, y, z, TiltStyle{GoogleWithLabel: true})
 }
